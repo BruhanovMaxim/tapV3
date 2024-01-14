8e645fb2c8aa72decbb233d856e53d991b57e458
BruhanovMaxim <73647295+BruhanovMaxim@users.noreply.github.com>
2024-01-14 20:00:35 +0300

    8
    8

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
+  // const slider_menu = document.querySelector(".button_menu");
 }
 
 function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
+}
+
+function showDialog() {
+  const dialog = document.getElementById("login-dialog");
+  dialog.showModal();
 }
